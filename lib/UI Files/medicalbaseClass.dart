dbd858b3b61c04671f363eeb8b2041626d9b6c7c
Haley Bonidie <bonidie.2@buckeyemail.osu.edu>
2024-10-19 12:30:47 -0400

    base class for medical created

--- lib/UI Files/medicalbaseClass.dart
+++ lib/UI Files/medicalbaseClass.dart
@@ -1,3 +1,28 @@
-class MedicalbaseClass {
-  
+import 'package:flutter/material.dart';
+
+class Medicalbaseclass extends StatefulWidget {
+  const Medicalbaseclass({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Medicalbaseclass> createState() => _SkeletonState();
+}
+
+class _SkeletonState extends State<Medicalbaseclass> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
 }
