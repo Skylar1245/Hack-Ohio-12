30120f48f593f1a931e60a7a93621eeef4caf688
Haley Bonidie <bonidie.2@buckeyemail.osu.edu>
2024-10-19 18:35:31 -0400

    update user input ui

--- lib/UI Files/UserInput.dart
+++ lib/UI Files/UserInput.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hackohio12/global_methods.dart';
 
 class UserInput extends StatefulWidget {
   const UserInput({super.key, required this.title});
@@ -12,15 +13,49 @@ class UserInput extends StatefulWidget {
 class _SkeletonState extends State<UserInput> {
   @override
   Widget build(BuildContext context) {
+    Decoration decoration = BoxDecoration(
+      gradient: Global.gradient(),
+      borderRadius: BorderRadius.circular(20), // Adjust the radius as needed
+    );
+
     return Scaffold(
+      backgroundColor: Global.background(),
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Global.appBar(),
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Text("hello world")],
+          children: <Widget>[
+            Container(
+                decoration: decoration,
+                width: Global.getWidthByPercentage(context, 90),
+                height: Global.getHeightByPercentage(context, 38),
+                child: Column(
+                  children: [
+                    Divider(height: 10, color: Colors.transparent),
+                    Text("Input Today's Data", style: TextStyle(
+                              color: Global.textColor(), fontSize: 24)),
+                              Divider(height: 5, color: Colors.transparent),
+                              Align(
+                          alignment: Alignment.topLeft,
+                          child: Row(children: [
+                            VerticalDivider(width: Global.getWidthByPercentage(context, 3)),
+                            Text("Which conditions have you had flare-ups of today?",
+                                style: TextStyle(
+                                  fontSize: Global.getWidthByPercentage(context, 3),
+                                  color: Global.textColor(),
+                                ))
+                          ]))
+                  ],
+                )),
+            Global.divider(context),
+            Container(
+                decoration: decoration,
+                width: Global.getWidthByPercentage(context, 90),
+                height: Global.getHeightByPercentage(context, 38))
+          ],
         ),
       ),
     );
