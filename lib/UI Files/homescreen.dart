dcf5388d7553a98dd03883f67fca16e6c592bd8d
kateG03 <kategoertz@outlook.com>
2024-10-19 12:43:00 -0400

    created initial compartments of homescreen and created first button (migraine)

--- lib/UI Files/homescreen.dart
+++ lib/UI Files/homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hackohio12/UI%20Files/medicalbaseClass.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.title});
@@ -17,11 +18,41 @@ class _SkeletonState extends State<HomeScreen> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            constraints: BoxConstraints.expand(
+              height:
+                  Theme.of(context).textTheme.headlineMedium!.fontSize! * 1.1 +
+                      200.0,
+            ),
+            padding: const EdgeInsets.all(8.0),
+            color: Colors.blue[600],
+            alignment: Alignment.bottomCenter,
+            child: Text('Im weather box',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineMedium!
+                    .copyWith(color: Colors.white)),
+          ),
+          Expanded(
+              child: Container(
+                  padding: const EdgeInsets.all(8.0),
+                  color: Colors.red[600],
+                  alignment: Alignment.bottomCenter,
+                  child: Column(children: <Widget>[
+                    FloatingActionButton.extended(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => const Medicalbaseclass(title: 'Medical')),
+                    );
+                      },
+                      label: Text('Migraines'),
+                      icon: Icon(Icons.thumb_down),
+                      backgroundColor: Colors.pink,
+                    )
+                  ])))
+        ],
       ),
     );
   }
