06834cb58f398320dae2dfac520c881b03cae7e6
vmjauregui3 <vmjauregui3@gmail.com>
2024-10-20 06:47:54 -0400

    Deleted Logs in weather_analysis

--- lib/Weather/weather_analysis.dart
+++ lib/Weather/weather_analysis.dart
@@ -25,44 +25,30 @@ class WeatherAnalysis {
     double humidityContribution = 0;
 
     for (int i = 0; i < 6; i++) {
-      //More than 5HPa pressure drop
-      log("Pressure Day ${5 + i} ${pressures[5 + i]} and Day ${4 + i} ${pressures[4 + i]}",
-          name: "WeatherAnalysis");
       num pressureChange = pressures[5 + i] - pressures[4 + i];
       if (pressureChange < -6) {
         pressureChangeContribution = 1;
       } else if (pressureChange < 0) {
         pressureChangeContribution = 1 - (pressureChange + 6) / 6;
       }
-      log("Pressure contribution $pressureChangeContribution",
-          name: "WeatherAnalysis");
 
-      log("Temperature Day ${5 + i} ${temperatures[5 + i]} and Day ${4 + i} ${temperatures[4 + i]}",
-          name: "WeatherAnalysis");
       num temperatureChange = temperatures[5 + i] - temperatures[4 + i];
       if (temperatureChange > 10) {
         temperatureContribution = 1;
       } else if (temperatureChange > 0) {
         temperatureContribution = temperatureChange / 10;
       }
-      log("Temperature contribution $temperatureContribution",
-          name: "WeatherAnalysis");
 
-      log("Humidity Day ${5 + i} ${humidities[5 + i]} and Day ${4 + i} ${humidities[4 + i]}",
-          name: "WeatherAnalysis");
       num humidityChange = humidities[5 + i] - humidities[4 + i];
       if (humidityChange > 25) {
         humidityContribution = 1;
       } else if (humidityChange > 0) {
         humidityContribution = humidityChange / 25;
       }
-      log("Humidity contribution $humidityContribution",
-          name: "WeatherAnalysis");
 
       migraineChances.add(100 * (0.65 * pressureChangeContribution +
           0.1 * temperatureContribution +
           0.25 * humidityContribution));
-      log("migraine chance: ${migraineChances[i]}", name: "WeatherAnalysis");
     }
 
     return migraineChances;
@@ -75,19 +61,13 @@ class WeatherAnalysis {
     double windSpeedContribution = 0;
 
     for (int i = 0; i < 6; i++) {
-      log("Temperature Day ${5 + i} ${temperatures[5 + i]} and Day ${4 + i} ${temperatures[4 + i]}",
-          name: "WeatherAnalysis");
       if (temperatures[5 + i] < 0) {
         temperatureContribution = 1;
       }
       if (temperatures[5 + i] <= 50) {
         temperatureContribution = (50 - temperatures[5 + i]) / 50;
       }
-      log("Temperature contribution $temperatureContribution",
-          name: "WeatherAnalysis");
 
-      log("Humidity Day ${5 + i} ${humidities[5 + i]} and Day ${4 + i} ${humidities[4 + i]}",
-          name: "WeatherAnalysis");
       if (humidities[5 + i] > 1) {
         humidityContribution = 1;
       }
@@ -96,23 +76,16 @@ class WeatherAnalysis {
       } else if (humidities[5 + i] < 30) {
         humidityContribution = (30 - humidities[5 + i]) / 0.3;
       }
-      log("Humidity contribution $humidityContribution",
-          name: "WeatherAnalysis");
       
-      log("Wind Speed Day ${5 + i} ${windSpeeds[5 + i]} and Day ${4 + i} ${windSpeeds[4 + i]}",
-          name: "WeatherAnalysis");
       if (windSpeeds[i+5]>30){
         windSpeedContribution = 1;
       } else if (windSpeeds[i+5] > 15) {
         windSpeedContribution = (windSpeeds[i+5] - 15)/15;
       }
-      log("Wind Speed contribution $windSpeedContribution",
-          name: "WeatherAnalysis");
 
       asthmaChances.add(100 * (0.5 * temperatureContribution +
           0.5 * humidityContribution) + 
           0.1 * windSpeedContribution);
-      log("asthma chance: ${asthmaChances[i]}", name: "WeatherAnalysis");
     }
 
     return asthmaChances;
@@ -126,9 +99,6 @@ class WeatherAnalysis {
     double windSpeedContribution = 0;
 
     for (int i = 0; i < 6; i++) {
-      //More than 5HPa pressure drop
-      log("Pressure Day ${5 + i} ${pressures[5 + i]} and Day ${4 + i} ${pressures[4 + i]}",
-          name: "WeatherAnalysis");
       num pressureChange = pressures[5 + i] - pressures[4 + i];
       if (pressureChange < -6) {
         pressureChangeContribution = 1;
@@ -139,30 +109,20 @@ class WeatherAnalysis {
       }else if (pressureChange > 6) {
         pressureChangeContribution = 1;
       }
-      log("Pressure contribution $pressureChangeContribution",
-          name: "WeatherAnalysis");
 
-      log("Temperature Day ${5 + i} ${temperatures[5 + i]} and Day ${4 + i} ${temperatures[4 + i]}",
-          name: "WeatherAnalysis");
       if (temperatures[5 + i] < 0) {
         temperatureContribution = 1;
       }
       if (temperatures[5 + i] < 40) {
         temperatureContribution = (40 - temperatures[5 + i]) / 40;
       }
-      log("Temperature contribution $temperatureContribution",
-          name: "WeatherAnalysis");
 
-      log("Humidity Day ${5 + i} ${humidities[5 + i]} and Day ${4 + i} ${humidities[4 + i]}",
-          name: "WeatherAnalysis");
       if (humidities[5 + i] > 1) {
         humidityContribution = 1;
       }
       if (humidities[5 + i] > 70) {
         humidityContribution = (humidities[5 + i] - 70) / 30;
       }
-      log("Humidity contribution $humidityContribution",
-          name: "WeatherAnalysis");
 
       if (windSpeeds[i+5]>30){
         windSpeedContribution = 1;
@@ -174,7 +134,6 @@ class WeatherAnalysis {
           0.25 * temperatureContribution +
           0.2 * humidityContribution + 
           0.1 * windSpeedContribution));
-      log("arthritis chance: ${arthritisChances[i]}", name: "WeatherAnalysis");
     }
 
     return arthritisChances;
