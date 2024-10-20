b4780729d522aba887d8ab78aaec2e2361763487
vmjauregui3 <vmjauregui3@gmail.com>
2024-10-20 04:38:26 -0400

    Asthma and Arthritis

--- lib/UI Files/barGraph.dart
+++ lib/UI Files/barGraph.dart
@@ -1,28 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class BarGraph extends StatefulWidget {
-  const BarGraph({super.key, required this.title});
+  const BarGraph({super.key, required this.flareUpChance});
 
-  final String title;
+  final List<double> flareUpChance;
 
   @override
   State<BarGraph> createState() => _BarGraph();
 }
 
 class _BarGraph extends State<BarGraph> {
+  int i = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
-      ),
-    );
+        body: Container(
+            color: Colors.yellow,
+            child: Center(
+                child: SfCartesianChart (
+                    //borderColor: Colors.purple,
+                    //borderWidth: 5,
+                    series: <CartesianSeries>[
+                  // Renders line chart
+                  BarSeries<double, int>(
+                      dataSource: widget.flareUpChance,
+                      color: Colors.black,
+                      width: 7,
+                      xValueMapper: (double d, int i) {
+                        return i;
+                      },
+                      yValueMapper: (double d, int i) {
+                        return widget.flareUpChance[i];
+                      })
+                ]))));
   }
 }
--- lib/Weather/weather_analysis.dart
+++ lib/Weather/weather_analysis.dart
@@ -3,36 +3,49 @@ import 'dart:developer';
 import 'package:hackohio12/global_methods.dart';
 
 class WeatherAnalysis {
-  static List<double> checkMigraineChances() {
-    //Past data
-    List<double> pressures = Global.weatherData.getPastPressures();
-    List<double> temperatures = Global.weatherData.getPastTemperatures();
-    List<double> humidities = Global.weatherData.getPastHumidities();
-    List<double> windSpeeds = Global.weatherData.getPastWindSpeeds();
-    List<double> precipitations = Global.weatherData.getPastPrecipitations();
-    // Future Predictions
+  //Past data
+  static List<double> pressures = Global.weatherData.getPastPressures();
+  static List<double> temperatures = Global.weatherData.getPastTemperatures();
+  static List<double> humidities = Global.weatherData.getPastHumidities();
+  static List<double> windSpeeds = Global.weatherData.getPastWindSpeeds();
+  static List<double> precipitations = Global.weatherData.getPastPrecipitations();
+
+  static void initialize(){
     pressures.addAll(Global.weatherData.getUpcomingPressures());
     temperatures.addAll(Global.weatherData.getUpcomingTemperatures());
     humidities.addAll(Global.weatherData.getUpcomingHumidities());
     windSpeeds.addAll(Global.weatherData.getUpcomingWindSpeeds());
     precipitations.addAll(Global.weatherData.getUpcomingPrecipitations());
 
+    // Future Data
+    pressures.addAll(Global.weatherData.getUpcomingPressures());
+    temperatures.addAll(Global.weatherData.getUpcomingTemperatures());
+    humidities.addAll(Global.weatherData.getUpcomingHumidities());
+    windSpeeds.addAll(Global.weatherData.getUpcomingWindSpeeds());
+    precipitations.addAll(Global.weatherData.getUpcomingPrecipitations());
+  }
+  
+  static List<double> checkMigraineChances() {
     List<double> migraineChances = List.empty(growable: true);
     List<double> pressureChangeContributions = List.empty(growable: true);
     List<double> temperatureContributions = List.empty(growable: true);
     List<double> humidityContributions = List.empty(growable: true);
-    //More than 500Pa pressure drop
+    
     for (int i = 0; i < 6; i++) {
-      log("message", name: "WeatherAnalysis");
+      //More than 5HPa pressure drop
+      log("Pressure Day ${5+i} ${pressures[5+i]} and Day ${4+i} ${pressures[4+i]}", name: "WeatherAnalysis");
       double pressureChange = pressures[5 + i] - pressures[4 + i];
-      if (pressureChange < -500) {
+      if (pressureChange < -5) {
         pressureChangeContributions.add(1);
       } else if (pressureChange < 0) {
-        pressureChangeContributions.add(1 - (pressureChange + 500) / 500);
+        pressureChangeContributions.add(1 - (pressureChange + 5) / 5);
       } else {
         pressureChangeContributions.add(0);
       }
+      log("Pressure contribution ${pressureChangeContributions[i]}", name: "WeatherAnalysis");
+      
 
+      log("Temperature Day ${5+i} ${temperatures[5+i]} and Day ${4+i} ${temperatures[4+i]}", name: "WeatherAnalysis");
       double temperatureChange = temperatures[5 + i] - temperatures[4 + i];
       if (temperatureChange > 10) {
         temperatureContributions.add(1);
@@ -41,21 +54,112 @@ class WeatherAnalysis {
       } else {
         temperatureContributions.add(0);
       }
+      log("Temperature contribution ${temperatureContributions[i]}", name: "WeatherAnalysis");
 
-      double humidityChange = humidities[5 + i] / humidities[4 + i];
-      if (humidityChange > 1.25) {
+      log("Humidity Day ${5+i} ${humidities[5+i]} and Day ${4+i} ${humidities[4+i]}", name: "WeatherAnalysis");
+      double humidityChange = humidities[5 + i] - humidities[4 + i];
+      if (humidityChange > 0.25) {
         humidityContributions.add(1);
-      } else if (humidityChange > 1) {
-        humidityContributions.add((humidityChange - 1) / 0.25);
+      } else if (humidityChange > 0) {
+        humidityContributions.add(humidityChange / 0.25);
       } else {
         humidityContributions.add(0);
       }
+      log("Humidity contribution ${humidityContributions[i]}", name: "WeatherAnalysis");
 
-      migraineChances.add(100*0.62 * pressureChangeContributions[i] +
+      migraineChances.add(100 * 0.65 * pressureChangeContributions[i] +
           0.1 * temperatureContributions[i] +
-          0.28 * humidityContributions[i]);
+          0.25 * humidityContributions[i]);
+      log("migraine chance: ${migraineChances[i]}", name: "WeatherAnalysis");
     }
 
     return migraineChances;
+  }
+
+  static List<double> getAsthmaChances() {
+    List<double> asthmaChances = List.empty(growable: true);
+    List<double> temperatureContributions = List.empty(growable: true);
+    List<double> humidityContributions = List.empty(growable: true);
+
+    for (int i = 0; i < 6; i++) {
+      log("Temperature Day ${5+i} ${temperatures[5+i]} and Day ${4+i} ${temperatures[4+i]}", name: "WeatherAnalysis");
+      if (temperatures[5+i] < 0) {
+        temperatureContributions.add(1);
+      } if (temperatures[5+i] < 50) {
+        temperatureContributions.add( (50-temperatures[5+i])/50 );
+      } else {
+        temperatureContributions.add(0);
+      }
+      log("Temperature contribution ${temperatureContributions[i]}", name: "WeatherAnalysis");
+
+      log("Humidity Day ${5+i} ${humidities[5+i]} and Day ${4+i} ${humidities[4+i]}", name: "WeatherAnalysis");
+      if (humidities[5+i] > 1) {
+        humidityContributions.add(1);
+      } if (humidities[5+i] > 0.7) {
+        humidityContributions.add( (humidities[5+i]-0.7)/0.3 );
+      } else if (humidities[5+i] < 0.3) {
+        humidityContributions.add( (0.3 - humidities[5+i]) / 0.3);
+      } else {
+        humidityContributions.add(0);
+      }
+      log("Humidity contribution ${humidityContributions[i]}", name: "WeatherAnalysis");
+
+      asthmaChances.add(100 * 0.5 * temperatureContributions[i] +
+          0.5 * humidityContributions[i]);
+      log("migraine chance: ${asthmaChances[i]}", name: "WeatherAnalysis");
+    }
+
+    return asthmaChances;
+  }
+
+  static List<double> getArthritisChances() {
+    List<double> arthritisChances = List.empty(growable: true);
+    List<double> pressureChangeContributions = List.empty(growable: true);
+    List<double> temperatureContributions = List.empty(growable: true);
+    List<double> humidityContributions = List.empty(growable: true);
+    
+    for (int i = 0; i < 6; i++) {
+      //More than 5HPa pressure drop
+      log("Pressure Day ${5+i} ${pressures[5+i]} and Day ${4+i} ${pressures[4+i]}", name: "WeatherAnalysis");
+      double pressureChange = pressures[5 + i] - pressures[4 + i];
+      if (pressureChange < -6) {
+        pressureChangeContributions.add(1);
+      } else if (pressureChange < 0) {
+        pressureChangeContributions.add(1 - (pressureChange + 5) / 5);
+      } else {
+        pressureChangeContributions.add(0);
+      }
+      log("Pressure contribution ${pressureChangeContributions[i]}", name: "WeatherAnalysis");
+      
+
+      log("Temperature Day ${5+i} ${temperatures[5+i]} and Day ${4+i} ${temperatures[4+i]}", name: "WeatherAnalysis");
+      double temperatureChange = temperatures[5 + i] - temperatures[4 + i];
+      if (temperatureChange > 10) {
+        temperatureContributions.add(1);
+      } else if (temperatureChange > 0) {
+        temperatureContributions.add(temperatureChange / 10);
+      } else {
+        temperatureContributions.add(0);
+      }
+      log("Temperature contribution ${temperatureContributions[i]}", name: "WeatherAnalysis");
+
+      log("Humidity Day ${5+i} ${humidities[5+i]} and Day ${4+i} ${humidities[4+i]}", name: "WeatherAnalysis");
+      double humidityChange = humidities[5 + i] - humidities[4 + i];
+      if (humidityChange > 0.25) {
+        humidityContributions.add(1);
+      } else if (humidityChange > 0) {
+        humidityContributions.add(humidityChange / 0.25);
+      } else {
+        humidityContributions.add(0);
+      }
+      log("Humidity contribution ${humidityContributions[i]}", name: "WeatherAnalysis");
+
+      arthritisChances.add(100 * 0.65 * pressureChangeContributions[i] +
+          0.1 * temperatureContributions[i] +
+          0.25 * humidityContributions[i]);
+      log("migraine chance: ${arthritisChances[i]}", name: "WeatherAnalysis");
+    }
+
+    return arthritisChances;
   }
 }
