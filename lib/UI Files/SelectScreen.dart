327f017197f0a18790ae8d831d21d67b684b635f
Haley Bonidie <bonidie.2@buckeyemail.osu.edu>
2024-10-19 23:33:26 -0400

    options state saved

--- lib/UI Files/SelectScreen.dart
+++ lib/UI Files/SelectScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hackohio12/global_methods.dart';
+import 'package:hackohio12/UserPref.dart';
 import 'package:hackohio12/UI%20Files/homescreen.dart';
 
 class SelectScreen extends StatefulWidget {
@@ -12,99 +13,125 @@ class SelectScreen extends StatefulWidget {
 }
 
 class _SkeletonState extends State<SelectScreen> {
+  void state(String str) {
+    setState(() {
+      if(str == "m") {
+      userPref.setMigraine(!userPref.migraine);
+      }
+      if(str == "as"){
+        userPref.setAsthma(!userPref.asthma);
+      }
+      if(str == "ar"){
+        userPref.setArthritis(!userPref.arthritis);
+      }
+    });
+  }
+
+  UserPref userPref = UserPref();
   @override
   Widget build(BuildContext context) {
     Decoration decoration = BoxDecoration(
       gradient: Global.gradient(),
       borderRadius: BorderRadius.circular(20), // Adjust the radius as needed
     );
-
-    return Scaffold(
-      backgroundColor: Global.background(),
-      appBar: AppBar(
-        backgroundColor: Global.appBar(),
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-                decoration: decoration,
-                width: Global.getWidthByPercentage(context, 90),
-                height: Global.getHeightByPercentage(context, 70),
-                child: Column(children: [
-                  Global.divider(context),
+    return FutureBuilder(
+        future: userPref.update(),
+        builder: (context, snapshot) {
+          if(snapshot.data == true) {
+          return Scaffold(
+            backgroundColor: Global.background(),
+            appBar: AppBar(
+              backgroundColor: Global.appBar(),
+              title: Text(widget.title),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
                   Container(
-                    width: Global.getWidthByPercentage(context, 80),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.transparent,
-                        border: Border.all(
-                            color: const Color.fromARGB(255, 255, 255, 255))),
-                    child: CheckboxListTile(
-                        checkColor: Global.tableData(),
-                        title: Text(
-                          "Migraine",
-                          style: TextStyle(
-                              color: Global.textColor(), fontSize: 20),
+                      decoration: decoration,
+                      width: Global.getWidthByPercentage(context, 90),
+                      height: Global.getHeightByPercentage(context, 70),
+                      child: Column(children: [
+                        Global.divider(context),
+                        Container(
+                          width: Global.getWidthByPercentage(context, 80),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.transparent,
+                              border: Border.all(
+                                  color: const Color.fromARGB(
+                                      255, 255, 255, 255))),
+                          child: CheckboxListTile(
+                              checkColor: Global.tableData(),
+                              title: Text(
+                                "Migraine",
+                                style: TextStyle(
+                                    color: Global.textColor(), fontSize: 20),
+                              ),
+                              value: userPref.migraine,
+                              onChanged: (value) {
+                                state("m");
+                              },
+                              controlAffinity: ListTileControlAffinity.leading),
                         ),
-                        value: true,
-                        onChanged: null,
-                        controlAffinity: ListTileControlAffinity.leading),
-                  ),
-                  Global.divider(context),
-                 Container(
-                    width: Global.getWidthByPercentage(context, 80),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.transparent,
-                        border: Border.all(
-                            color: const Color.fromARGB(255, 255, 255, 255))),
-                    child: CheckboxListTile(
-                        checkColor: Global.tableData(),
-                        title: Text(
-                          "Asthma",
-                          style: TextStyle(
-                              color: Global.textColor(), fontSize: 20),
+                        Global.divider(context),
+                        Container(
+                          width: Global.getWidthByPercentage(context, 80),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.transparent,
+                              border: Border.all(
+                                  color: const Color.fromARGB(
+                                      255, 255, 255, 255))),
+                          child: CheckboxListTile(
+                              checkColor: Global.tableData(),
+                              title: Text(
+                                "Asthma",
+                                style: TextStyle(
+                                    color: Global.textColor(), fontSize: 20),
+                              ),
+                              value: userPref.asthma,
+                              onChanged: (value){state("as");},
+                              controlAffinity: ListTileControlAffinity.leading),
                         ),
-                        value: false,
-                        onChanged: null,
-                        controlAffinity: ListTileControlAffinity.leading),
-                  ),
-                  Global.divider(context),
-                  Container(
-                    width: Global.getWidthByPercentage(context, 80),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.transparent,
-                        border: Border.all(
-                            color: const Color.fromARGB(255, 255, 255, 255))),
-                    child: CheckboxListTile(
-                        checkColor: Global.tableData(),
-                        title: Text(
-                          "Arthritis",
-                          style: TextStyle(
-                              color: Global.textColor(), fontSize: 20),
+                        Global.divider(context),
+                        Container(
+                          width: Global.getWidthByPercentage(context, 80),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.transparent,
+                              border: Border.all(
+                                  color: const Color.fromARGB(
+                                      255, 255, 255, 255))),
+                          child: CheckboxListTile(
+                              checkColor: Global.tableData(),
+                              title: Text(
+                                "Arthritis",
+                                style: TextStyle(
+                                    color: Global.textColor(), fontSize: 20),
+                              ),
+                              value: userPref.arthritis,
+                              onChanged: (value){state("ar");},
+                              controlAffinity: ListTileControlAffinity.leading),
                         ),
-                        value: true,
-                        onChanged: null,
-                        controlAffinity: ListTileControlAffinity.leading),
-                  ),
-                  Global.divider(context),
-                  FloatingActionButton(onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        HomeScreen(title: 'Home Screen'),
-                                  ),
-                                );
-                              })
-                ]))
-          ],
-        ),
-      ),
-    );
+                        Global.divider(context),
+                        FloatingActionButton(onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  HomeScreen(title: 'Home Screen'),
+                            ),
+                          );
+                        })
+                      ]))
+                ],
+              ),
+            ),
+          );} else {
+            return Scaffold(body: Center(child: CircularProgressIndicator()));
+          }
+        });
   }
 }
--- lib/UserPref.dart
+++ lib/UserPref.dart
@@ -1,5 +1,56 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-class UserPref{
-  
+class UserPref {
+  bool migraine = false;
+  bool asthma = false;
+  bool arthritis = false;
+
+  Future<bool> update() async{
+    migraine = await getMigraine();
+    asthma = await getAsthma();
+    arthritis = await getArthritis();
+    return true;
+  }
+
+
+
+  Future<void> setMigraine(bool migraine) async {
+    final preferences = await SharedPreferences.getInstance();
+
+    await preferences.setBool('migraine', migraine);
+  }
+
+  Future<bool> getMigraine() async {
+    final preferences = await SharedPreferences.getInstance();
+
+    return preferences.getBool('migraine') ?? false;
+  }
+
+
+
+  Future<void> setAsthma(bool asthma) async {
+    final preferences = await SharedPreferences.getInstance();
+
+    await preferences.setBool('asthma', asthma);
+  }
+
+  Future<bool> getAsthma() async {
+    final preferences = await SharedPreferences.getInstance();
+
+    return preferences.getBool('asthma') ?? false;
+  }
+
+
+
+  Future<void> setArthritis(bool arthritis) async {
+    final preferences = await SharedPreferences.getInstance();
+
+    await preferences.setBool('arthritis', arthritis);
+  }
+
+  Future<bool> getArthritis() async {
+    final preferences = await SharedPreferences.getInstance();
+
+    return preferences.getBool('arthritis') ?? false;
+  }
 }
