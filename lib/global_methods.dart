e5ca8cd5f016a4f389031d59fa892d83ff5ecdd7
kateG03 <kategoertz@outlook.com>
2024-10-20 03:31:00 -0400

    everything is set up for asthma and arthritis; waiting on backend

--- lib/global_methods.dart
+++ lib/global_methods.dart
@@ -78,9 +78,11 @@ class Global {
         List<double> migraineList = WeatherAnalysis.checkMigraineChances();
         return migraineList[0].toInt();
       case MedicalCondition.asthma:
-        return 20;
+        List<double> asthmaList = WeatherAnalysis.checkMigraineChances();
+        return asthmaList[0].toInt(); //FIXME
       case MedicalCondition.arthritis:
-        return 30;
+        List<double> arthritisList = WeatherAnalysis.checkMigraineChances();
+        return arthritisList[0].toInt(); //FIXME
       default:
         return 0;
     }
@@ -98,14 +100,35 @@ class Global {
     }
   }
 
+  static String evalAsthma() {
+    int x = percent(MedicalCondition.asthma);
+    if (x < 25) {
+      return "There is a low risk for asthma attacks today based on weather patterns. Always keep an eye on the weather for your health, as there is always a chance the weather could turn.";
+    } else if (x >= 25 && x < 75) {
+      return "There is a medium risk for asthma attacks today based on weather patterns. Make sure to have your inhaler on your person and be wary of any drops in air quality.";
+    } else {
+      return "There is a high risk for asthma attacks today based on weather patterns. Try to limit your time outside and make sure to have your inhaler on your person.";
+    }
+  }
+
+static String evalArthritis() {
+    int x = percent(MedicalCondition.arthritis);
+    if (x < 25) {
+      return "There is a low risk for arthritis flare-ups today based on weather patterns. To help prevent future flare-ups, be sure to exercise regularly within your physical abilities.";
+    } else if (x >= 25 && x < 75) {
+      return "There is a medium risk for arthritis flare-ups today based on weather patterns. Bring any protective gear or medications with you to ensure you are prepared if a flare-up occurs.";
+    } else {
+      return "There is a high risk for arthritis flare-ups today based on weather patterns. In the event of a flare up, be sure to wear any protective braces and take any prescribed medications in order to lessen pain.";
+    }
+}
   static String description(MedicalCondition medicalCond) {
     switch (medicalCond) {
       case MedicalCondition.migraine:
         return evalMigraine();
       case MedicalCondition.asthma:
-        return "info about asthma";
+        return evalAsthma();
       case MedicalCondition.arthritis:
-        return "info about arthritis";
+        return evalArthritis();
       default:
         return ":(";
     }
